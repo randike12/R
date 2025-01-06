7c1b25e7b72c575b6f33cbe62762fff4fc03145c
randike12 <rasaurabh1144@gmail.com>
2025-01-06 11:41:21 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7655786078:AAHYocIn3_vOC-dELlfWxqtcdPPkhJvEeAo")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAJPUTDDOS")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1821595166")
+
+
+
+
+  
